3d95eb7604d5f8095ce14b8ac14043fd1ee0666b
kawinnat09 <Kwinnathmayura@gmail.com>
2022-08-18 17:45:20 +0700

    first commit

--- lib/list_view_menu.dart
+++ lib/list_view_menu.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
 
+class ListMenu{
+  final String? title;
+  final String? subtitle;
+  final IconData? icon;
+  const ListMenu({this.title,this.subtitle,this.icon});
+}
+
+List<ListMenu>menus=[
+  const ListMenu(title: 'MENU-1',subtitle: 'SUB-1',icon: Icons.person),
+  const ListMenu(title: 'MENU-2',subtitle: 'SUB-2',icon: Icons.person_add),
+  const ListMenu(title: 'MENU-3',subtitle: 'SUB-3',icon: Icons.wifi),
+];
+
 class ListViewMenu extends StatelessWidget {
   const ListViewMenu({Key? key}) : super(key: key);
 
@@ -14,18 +27,18 @@ class ListViewMenu extends StatelessWidget {
           padding: EdgeInsets.all(8.0),
           itemCount: 4,
           itemBuilder: (context, index) {
-            return Divider(color: Colors.red);
+            return Divider(color: Colors.lightGreen,);
           },
           separatorBuilder: (context, index) {
             return ListTile(
-              title: Text("MENU-1"),
-              subtitle: Text("Sub-1"),
+              title: Text('${menus[index].title}'),
+              subtitle: Text("${menus[index].subtitle}"),
               leading: CircleAvatar(
-                child: Icon(Icons.person),
+                child: Icon(menus[index].icon),
               ),
               trailing: Icon(Icons.arrow_right),
-              onTap: () {
-                debugPrint("MENU-1");
+              onTap: (){
+                debugPrint(menus[index].title);
               },
             );
           },
